74e470a64a2db28268d5c9824ee58b5fa771afd5
Anthony <111950318@castrocarazo.ac.cr>
2023-05-08 19:30:57 -0600

    Commit Anthony

--- src/front/js/pages/login.jsx
+++ src/front/js/pages/login.jsx
@@ -13,6 +13,11 @@ const Login = () => {
   const [password, setPassword] = useState("");
   const navigate = useNavigate();
 
+
+
+
+
+
   const handleLogin = async (e) => {
     e.preventDefault();
     if (!email || !password) {
